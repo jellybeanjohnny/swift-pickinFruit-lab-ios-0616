4fe4a211f6cfd8092c5e51787ae7dd361fe70ee7
jellybeanjohnny <mamerige@icloud.com>
2016-07-22 19:02:22 -0400

    completed lab

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -9,23 +9,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBOutlet weak var fruitPicker: UIPickerView!
+    
     
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
+        self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
+        fruitPicker.delegate = self
+        
         
     }
     
     @IBAction func spinButtonTapped(sender: UIButton) {
         
+        for index in 0..<3 {
+            let randomNumber = Int(arc4random_uniform(UInt32(fruitsArray.count)))
+            fruitPicker.selectRow(randomNumber, inComponent: index, animated: true)
+        }
         
+        let firstSelected = fruitPicker.selectedRowInComponent(0)
+        let secondSelected = fruitPicker.selectedRowInComponent(1)
+        let thirdSelected = fruitPicker.selectedRowInComponent(2)
+        
+        if firstSelected == secondSelected && secondSelected == thirdSelected {
+            resultLabel.text = "WINNER!"
+        }
+        else {
+            resultLabel.text = "TRY AGAIN"
+        }
     }
     
 }
@@ -48,6 +67,28 @@ extension ViewController {
     }
     
 }
+
+// MARK: UIPIckerView Datasource
+extension ViewController: UIPickerViewDataSource {
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return fruitsArray.count
+    }
+    
+}
+
+// MARK: UIPickerview Delegate
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return fruitsArray[row]
+    }
+}
+
+
 
 
 
